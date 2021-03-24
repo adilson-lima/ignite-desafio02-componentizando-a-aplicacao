8d5510fe3481ae20ba34410eed4ccd9a8ec8dda7
Adilson Pereira Lima <adilson.pereira.lima@gmail.com>
2021-03-24 13:45:38 -0300

    Componentizacao da Sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,35 +1,23 @@
 import { useEffect, useState } from 'react';
 import { Button } from '../components/Button';
 
-import { api } from '../services/api';
-
 import '../styles/sidebar.scss';
 
-interface GenreResponseProps {
+interface Genre {
   id: number;
   name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
   title: string;
 }
 
+interface GenreResponseProps {
+  genres: Genre[];
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
 
 
-export function SideBar() {
+export function SideBar({ genres, selectedGenreId, handleClickButton }: GenreResponseProps) {
 
-  const [selectedGenreId, setSelectedGenreId] = useState(1);
-
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
-
-  useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
-      setGenres(response.data);
-    });
-  }, []);
-
-
-  function handleClickButton(id: number) {
-    setSelectedGenreId(id);
-  }
 
   return (
     <nav className="sidebar">
